e87fdfcf56bbf70f50061f676d87c53cdeffabd0
manavpanchal <manav03panchal@gmail.com>
2025-05-28 13:40:30 -0600

    cstdlib from scratch

--- common.c
+++ common.c
@@ -62,3 +62,50 @@ void printf(const char *fmt, ...) {
 end:
   va_end(vargs);
 }
+
+// The memcpy function copies n bytes from src to dst
+void *memcpy(void *dst, const void *src, size_t n) {
+  uint8_t *d = (uint8_t *)dst;
+  const uint8_t *s = (const uint8_t *)src;
+  while (n--) {
+    *d++ = *s++;
+  }
+  return dst;
+}
+
+// The memset function fills the first n bytes of buf with c.
+void *memset(void *buf, char c, size_t n) {
+  uint8_t *p = (uint8_t *)buf;
+  while (n--) {
+    *p++ = c;
+  }
+  return buf;
+}
+
+// TODO: implement strcpy_s (The strcpy function continues copying even if src
+// is longer than the memory area of dst. This can easily lead to bugs and
+// vulnerabilities.)
+// This function copies the string from src to dst
+char *strcpy(char *dst, const char *src) {
+  char *d = dst;
+  while (*src) {
+    *d++ = *src++;
+  }
+  *d = '\0';
+  return dst;
+}
+
+// The strcmp function is often used to check if two strings are identical.
+// It's a bit counter-intuitive, but the strings are identical when !strcmp(s1,
+// s2) is true (i.e., when the function returns zero):
+int strcmp(const char *s1, const char *s2) {
+  while (*s1 && *s2) {
+    if (*s1 != *s2) {
+      break;
+    }
+    s1++;
+    s2++;
+  }
+  // This is to be POSIX compliant(?)
+  return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
