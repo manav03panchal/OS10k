3d625e0cb13e8ad8672a8d8da13a6536612febef
manavpanchal <manav03panchal@gmail.com>
2025-05-29 22:00:37 -0600

    kernel panic and some code moving

--- kernel.h
+++ kernel.h
@@ -1,5 +1,14 @@
 #pragma once
 
+// It prints where the panic occurred, it enters an infinite loop to halt
+// processing.
+#define PANIC(fmt, ...)                                                        \
+  do {                                                                         \
+    printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);      \
+    while (1) {                                                                \
+    }                                                                          \
+  } while (0)
+
 struct sbiret {
   long error;
   long value;
